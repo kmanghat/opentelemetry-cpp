d8eb37cad170af235138d7ed0297460b5323aae1
Janet Vu <vjanet@umich.edu>
2020-06-29 18:07:26 +0000

    Change raw pointers to unique, change variable names

--- ext/src/zpages/tracez_processor.cc
+++ ext/src/zpages/tracez_processor.cc
@@ -5,24 +5,24 @@ namespace ext {
 namespace zpages {
 
   void TracezSpanProcessor::OnStart(opentelemetry::sdk::trace::Recordable &span) noexcept {
-    RunningSpans.insert(&span);
+    running_spans.insert(std::unique_ptr<opentelemetry::sdk::trace::Recordable>(&span));
   }
 
   void TracezSpanProcessor::OnEnd(std::unique_ptr<opentelemetry::sdk::trace::Recordable> &&span) noexcept {
      if (!IsSampled) return;
-     auto completedSpan = RunningSpans.find(span.get());
-     if (completedSpan != RunningSpans.end()) {
-       CompletedSpans.insert(*completedSpan);
-       RunningSpans.erase(completedSpan);
+     auto completed_span = running_spans.find(span.get());
+     if (completed_span != running_spans.end()) {
+       completed_spans.insert(*completed_span);
+       running_spans.erase(completed_span);
      }
   }
 
-  std::unordered_set<opentelemetry::sdk::trace::Recordable*> TracezSpanProcessor::GetRunningSpans() noexcept {
-    return RunningSpans;
+  std::unordered_set<std::unique_ptr<opentelemetry::sdk::trace::Recordable>> TracezSpanProcessor::GetRunningSpans() noexcept {
+    return running_spans;
   }
 
-  std::unordered_set<opentelemetry::sdk::trace::Recordable*> TracezSpanProcessor::GetCompletedSpans() noexcept {
-    return CompletedSpans;
+  std::unordered_set<std::unique_ptr<opentelemetry::sdk::trace::Recordable>> TracezSpanProcessor::GetCompletedSpans() noexcept {
+    return completed_spans;
   }
 
 
