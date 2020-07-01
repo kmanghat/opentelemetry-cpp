b36ec927f620463c4a7c057c15f2e53ef51a91dd
Janet Vu <vjanet@umich.edu>
2020-07-01 14:12:56 +0000

    Add basic counting tests

--- ext/test/zpages/tracez_processor_test.cc
+++ ext/test/zpages/tracez_processor_test.cc
@@ -1,6 +1,7 @@
-#include "opentelemetry/ext/zpages/tracez_processoor.h"
+#include "opentelemetry/ext/zpages/tracez_processor.h"
 #include "opentelemetry/nostd/span.h"
 #include "opentelemetry/sdk/trace/span_data.h"
+#include "opentelemetry/sdk/trace/tracer.h"
 
 #include <gtest/gtest.h>
 
@@ -14,8 +15,7 @@ class MockSpanExporter final : public SpanExporter {
  public:
   MockSpanExporter(std::shared_ptr<bool> span_received,
                    std::shared_ptr<bool> shutdown_called) noexcept
-      : span_received_(span_received), shutdown_called_(shutdown_called)
-  {}
+      : span_received_(span_received), shutdown_called_(shutdown_called) {}
 
   std::unique_ptr<Recordable> MakeRecordable() noexcept override {
     return std::unique_ptr<Recordable>(new SpanData);
@@ -61,21 +61,60 @@ TEST(TracezSpanProcessor, ToMockSpanExporter) {
 }
 
 
-
-TEST(TracezSpanProcessor, ToMockSpanExporter) {
+TEST(TracezSpanProcessor, NoSpans) {
   std::shared_ptr<bool> span_received(new bool(false));
   std::shared_ptr<bool> shutdown_called(new bool(false));
   std::unique_ptr<SpanExporter> exporter(new MockSpanExporter(span_received, shutdown_called));
-  TracezSpanProcessor processor(std::move(exporter));
+  std::shared_ptr<TracezSpanProcessor> processor(new TracezSpanProcessor(std::move(exporter)));
 
-  auto recordable = processor.MakeRecordable();
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 0);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 0);
 
-  processor.OnStart(*recordable);
-  ASSERT_FALSE(*span_received);
-
-  processor.OnEnd(std::move(recordable));
-  ASSERT_TRUE(*span_received);
-
-  processor.Shutdown();
-  ASSERT_TRUE(*shutdown_called);
 }
+
+
+
+TEST(TracezSpanProcessor, OneSpanRightContainer) {
+  std::shared_ptr<bool> span_received(new bool(false));
+  std::shared_ptr<bool> shutdown_called(new bool(false));
+  std::unique_ptr<SpanExporter> exporter(new MockSpanExporter(span_received, shutdown_called));
+  std::shared_ptr<TracezSpanProcessor> processor(new TracezSpanProcessor(std::move(exporter)));
+  auto tracer = std::shared_ptr<Tracer>(new Tracer(processor));
+
+  auto span = tracer->StartSpan("span");
+
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 0);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 1);
+
+  span->End();
+
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 1);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 0);
+
+}
+
+
+TEST(TracezSpanProcessor, MultipleSpansRightContainer) {
+  std::shared_ptr<bool> span_received(new bool(false));
+  std::shared_ptr<bool> shutdown_called(new bool(false));
+  std::unique_ptr<SpanExporter> exporter(new MockSpanExporter(span_received, shutdown_called));
+  std::shared_ptr<TracezSpanProcessor> processor(new TracezSpanProcessor(std::move(exporter)));
+  auto tracer = std::shared_ptr<Tracer>(new Tracer(processor));
+
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 0);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 0);
+
+  auto span1 = tracer->StartSpan("span");
+  auto span2 = tracer->StartSpan("span");
+
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 0);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 2);
+
+  span1->End();
+  span2->End();
+
+  ASSERT_EQ(processor->GetCompletedSpans().size(), 2);
+  ASSERT_EQ(processor->GetRunningSpans().size(), 0);
+
+}
+
