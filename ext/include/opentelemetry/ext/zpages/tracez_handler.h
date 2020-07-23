2693c213f746745678fa779f9f7d04a56de49009
Keshav Manghat <keshavmanghat@gmail.com>
2020-07-23 18:11:15 +0000

    Added Getters for error and latency spans

--- ext/include/opentelemetry/ext/zpages/tracez_handler.h
+++ ext/include/opentelemetry/ext/zpages/tracez_handler.h
@@ -24,136 +24,66 @@ namespace zpages
 
 class TracezHandler {
  public:
-
-  std::string GetRandomString() {
-    std::string s = "";
-    const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < rand() % 8 + 2; ++i) {
-        s += alphanum[rand() % (sizeof(alphanum) - 1)];
-    }
-    return s;
-  }
-
-  std::string GetRandomVectorItem(std::vector<std::string> v){
-    return v[rand() % v.size()];
-  }
-
-  std::string GetDescriptionString() {
-    std::string base = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    int start = rand() % 20;
-    int end = rand() % (base.length() - start);
-    return base.substr(start, end);
-  }
-
-  int GetRandomTimeInt() {
-    return rand() % 999999 + 10000000000;
-  }
-
-  json RunningMockData(int upper = 5, int lower = 0) {
-    std::vector<std::string> parent_ids;
-    for (int i = 0; i < rand() % 2 + 1; i++) {
-      parent_ids.push_back(GetRandomString());
-    }
-    json temp = json::array();
-    for (int i = 0; i < rand() % upper + lower; i++) {
-      temp.push_back({
-        {"spanid", GetRandomString()},
-        {"parentid", GetRandomVectorItem(parent_ids)},
-        {"traceid", GetRandomString()},
-        {"description", GetDescriptionString()},
-        {"start", GetRandomTimeInt()},
-      });
-    }
-    return temp;
-  }
-
-  json ErrorMockData(int upper = 5, int lower = 0) {
-    std::vector<std::string> error_statuses {"Canceled", "Unknown", "NotFound"};
-    std::vector<std::string> parent_ids;
-    for (int i = 0; i < rand() % 2 + 1; i++) {
-      parent_ids.push_back(GetRandomString());
-    }
+  json LatencySampleSpansJSON(const std::string& name, int latency_bucket){
     auto temp = json::array();
-    for (int i = 0; i < rand() % upper + lower; i++) {
-      temp.push_back({
-        {"spanid", GetRandomString()},
-        {"parentid", GetRandomVectorItem(parent_ids)},
-        {"traceid", GetRandomString()},
-        {"status", GetRandomVectorItem(error_statuses)},
-        {"description", GetDescriptionString()},
-        {"start", GetRandomTimeInt()},
-      });
-    }
-    return temp;
-  }
-
-  json LatencyMockData(int upper = 5, int lower = 0) {
-    std::vector<std::string> parent_ids;
-    for (int i = 0; i < rand() % 2 + 1; i++) {
-      parent_ids.push_back(GetRandomString());
-    }
-    auto temp = json::array();
-    for (int i = 0; i < rand() % upper + lower; i++) {
-      auto start_time = GetRandomTimeInt();
-      auto end_time = start_time + (rand() % 200 + 1);
-      temp.push_back({
-        {"spanid", GetRandomString()},
-        {"parentid", GetRandomVectorItem(parent_ids)},
-        {"traceid", GetRandomString()},
-        {"start", start_time},
-        {"end", end_time},
-        {"description", GetDescriptionString()},
-        {"duration", end_time - start_time},
-      });
-    }
-    return temp;
-  }
-
-  json CountsMockData(int upper = 5, int lower = 0) {
-    auto temp = json::array();
-    for (int i = 0; i < rand() % upper + lower; i++) {
-      auto latency = json::array();
-      for (int j = 0; j < 9; j++) {
-        latency.push_back({rand() % 100});
+    
+    if(aggregated_data_.find(name) != aggregated_data_.end()){
+      const auto &latency_samples = aggregated_data_[name].sample_latency_spans[latency_bucket];
+      for(const auto &latency_sample : latency_samples){
+        temp.push_back({
+          {"spanid", latency_sample.span_id},
+          {"parentid", latency_sample.parent_id},
+          {"traceid", latency_sample.trace_id},
+          {"description", latency_sample.description},
+          {"start", latency_sample.start_time},
+          {"duration", latency_sample.duration},
+        });
       }
-      temp.push_back({
-        {"name", GetRandomString()},
-        {"error", rand() % 100},
-        {"running", rand() % 100},
-        {"latency", latency}
-      });
     }
+    
     return temp;
   }
-
 
   void updateAggregations() {
     aggregated_data_ = data_aggregator_->GetAggregatedTracezData();
   }
-
-  json GetRunningJSON(const std::string& name) {
+  
+  json GetErrorSampleSpanJSON(const std::string& name){
     auto temp = json::array();
-    auto grouping = aggregated_data_.find(name);
-
-    if (grouping != aggregated_data_.end()) {
-      const auto &running_samples = grouping->second.sample_running_spans;
-      for (const auto &sample : running_samples){
+    
+    if(aggregated_data_.find(name) != aggregated_data_.end()){
+      const auto &error_samples = aggregated_data_[name].sample_error_spans;
+      for(const auto &error_sample : error_samples){
         temp.push_back({
-          {"spanid", sample.span_id},
-          {"parentid", sample.parent_id},
-          {"traceid", sample.trace_id},
-          {"description", sample.description},
-          {"start", sample.start_time},
+          {"spanid", error_sample.span_id},
+          {"parentid", error_sample.parent_id},
+          {"traceid", error_sample.trace_id},
+          {"description", error_sample.description},
+          {"start", error_sample.start_time},
+        });
+      }
+    }
+    return temp;
+  }
+  
+  json GetRunningSampleSpanJSON(const std::string& name) {
+    auto temp = json::array();
+    if(aggregated_data_.find(name) != aggregated_data_.end()){
+      const auto &running_samples = aggregated_data_[name].sample_running_spans;
+      for(const auto &running_sample : running_samples){
+        temp.push_back({
+          {"spanid", running_sample.span_id},
+          {"parentid", running_sample.parent_id},
+          {"traceid", running_sample.trace_id},
+          {"description", running_sample.description},
+          {"start", running_sample.start_time},
         });
       }
     }
     return temp;
   }
 
-  json AggregationData() {
+  json GetAggregationSummary() {
     updateAggregations();
     auto temp = json::array();
     for(const auto &aggregation_group: aggregated_data_){
@@ -217,27 +147,26 @@ class TracezHandler {
       std::string query = GetSuffix(req.uri);
       if (StartsWith(query, "latency")) {
         auto queried_bucket_name = GetAfterSlash(query);
-        auto queried_latency_index = GetBeforeSlash(queried_bucket_name);
+        auto queried_latency_index = std::stoi(GetBeforeSlash(queried_bucket_name));
         auto queried_name = GetAfterSlash(queried_bucket_name);
-        resp.body = LatencyMockData().dump();
+        resp.body = LatencySampleSpansJSON(queried_name, queried_latency_index).dump();
       }
       else {
         auto queried_name = GetAfterSlash(query);
 
         if (StartsWith(query, "aggregations")) {
-          resp.body = AggregationData().dump();
+          resp.body = GetAggregationSummary().dump();
         }
         else if (StartsWith(query, "running")) {
-          //resp.body = RunningMockData().dump();
-          resp.body = GetRunningJSON(queried_name).dump();
+          resp.body = GetRunningSampleSpanJSON(queried_name).dump();
         }
         else if (StartsWith(query, "error")) {
-          resp.body = ErrorMockData().dump();
+          resp.body = GetErrorSampleSpanJSON(queried_name).dump();
         }
       }
     }
     else {
-      resp.body = AggregationData().dump();
+      resp.body = GetAggregationSummary().dump();
     };
     return 200;
   }};
