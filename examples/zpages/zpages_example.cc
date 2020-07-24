3afe1bd3e9e3e7e404d0f635ddd0bca574ff2fa1
Keshav Manghat <keshavmanghat@gmail.com>
2020-07-24 02:56:14 +0000

    Added to zpages example

--- examples/zpages/zpages_example.cc
+++ examples/zpages/zpages_example.cc
@@ -8,21 +8,46 @@
 
 #include "opentelemetry/ext/zpages/zpages.h"
 
+using opentelemetry::core::SteadyTimestamp;
+namespace nostd = opentelemetry::nostd;
+using opentelemetry::v0::trace::Span;
+
 int main(int argc, char* argv[]) {
   zPages();
-
+  auto tracer = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("");
+  std::vector<nostd::unique_ptr<Span>> running_span_container;
   while (1) {
-    auto t = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("");
-    t->StartSpan("span1");
-    for (int i = 0; i < 50; i++) {
-      auto tracer = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("");
-      tracer->StartSpan("span1");
-      std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 10000 + 100));
-      tracer->StartSpan("span2");
-      std::cout << i << " ";
+    std::string span_name;
+    std::cout << "Enter span name: ";
+    std::cin >> span_name;
+    
+    char span_type;
+    std::cout << "Enter span type Error(E), Completed(C), Running(R)";
+    std::cin >> span_type;
+    
+    if(span_type == 'R'){
+      running_span_container.push_back(tracer->StartSpan(span_name));
     }
-    std::cout << "Press <ENTER> for more spans\n";
-    std::cin.get();
+    else if(span_type == 'C'){
+      int start_time;
+      std::cout << "Start time in nanoseconds: ";
+      std::cin >> start_time;
+      
+      int end_time;
+      std::cout << "End time in nanoseconds: ";
+      std::cin >> end_time;
+      
+      opentelemetry::trace::StartSpanOptions start;
+      start.start_steady_time = SteadyTimestamp(nanoseconds(start_time));
+      opentelemetry::trace::EndSpanOptions end;
+      end.end_steady_time = SteadyTimestamp(nanoseconds(end_time));
+      tracer->StartSpan(span_name,start)->End(end);
+    } else {
+      tracer->StartSpan(span_name)
+      ->SetStatus(opentelemetry::trace::CanonicalCode::CANCELLED,
+                  "span cancelled");
+    }
+    
   }
   std::cout << "Presss <ENTER> to stop...\n";
   std::cin.get();
